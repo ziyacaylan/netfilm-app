b68067b499a35517cdd590902cbf6e5a68af1752
ziyacaylan <ziyacaylan@gmail.com>
2023-01-25 10:03:23 +0300

    work on MovieDetail page

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { notFound } from "next/navigation";
+
+import MovieContainer from "@/containers/movie";
+import Movies from "@/mocks/movies.json";
+
+function MoviePage({ params }) {
+  const movieDetail = Movies.results.find(
+    (movie) => movie.id.toString() === params.id
+  );
+  //console.log(params);
+  //console.log(movieDetail);
+
+  if (!movieDetail) {
+    notFound();
+  }
+
+  return <MovieContainer movie={movieDetail} />;
+}
+
+export default MoviePage;
---
+++ containers/movie/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FeaturedMovie from "@/components/featured-movie";
+
+function MovieContainer({ movie }) {
+  return <FeaturedMovie movie={movie} isCompact={false} />;
+}
+
+export default MovieContainer;
